80f17005c07832f3a2be4f97f46cde1692788ffe
aferatovic <aferatovic@fordham.edu>
2022-02-15 20:24:31 -0500

    added pythoncode1 file

---
+++ pythoncode1.py
@@ -0,0 +1,10 @@
+a = (input('Would you like a greeting (yes or no)? '))
+if a == 'yes':
+    print('Good morning!')
+elif a == 'no':
+    print('Okay, goodbye!')
+else:
+    print('Please try again.')
+ 
+Would you like a greeting (yes or no)? yes
+Good morning!
